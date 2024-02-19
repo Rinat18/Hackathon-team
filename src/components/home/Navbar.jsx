e78ddc27e1d381461cbec7c843ea7c2d2b7c8db9
iskander bijnazarov <iskanderbijnazarov1@gmail.com>
2024-02-19 15:40:19 +0600

    navbar

--- src/components/home/Navbar.jsx
+++ src/components/home/Navbar.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import logo from "../../images/image 910.png"
+import logo1 from "../../images/Group 5287.png"
+import basket from "../../images/Vector.png"
+import vector from "../../images/Vector 3063 (Stroke).png"
+import lupa from "../../images/Union.png"
 
-export default function Navbar() {
+
+const Navbar = () => {
   return (
     <div>
+      <div className='navbar'>
+        <img className='logo' src={logo} alt="" />
+        <div className='playnchill'>
+        Playnchill
+        </div>
+        <img className='logo1' src={logo1} alt="" />
+        <input className='input' type="text" placeholder='Поиск'/>
+        <div className='besplatno'>
+          бесплатно
+        </div>
+        <a href=""><img src={vector} alt="" /></a>
+        <a href=""><img src={basket} alt="" /></a>
+        
+
+      </div>
       
     </div>
   )
 }
+
+export default Navbar
