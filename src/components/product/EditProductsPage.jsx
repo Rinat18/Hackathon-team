4fa6469a5d3a8ea3d25bf054744066ef2049dea4
Kanat Kaldarov <kanatkaldarow@gmail.com>
2024-02-19 22:16:15 +0600

    rinat chmo

--- src/components/product/EditProductsPage.jsx
+++ src/components/product/EditProductsPage.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { UseProduct } from "../../context/ProductContextProvider";
 
 export default function EditProductsPage() {
+  const { id } = useParams();
+  const { getOneProduct, oneProduct, editProduct } = UseProduct();
+
+  const [product, setProduct] = useState({
+    title: "",
+    description: "",
+    category: "",
+    price: 0,
+    image: "",
+  });
+  const handleInput = (e) => {
+    if (e.target.name === "price") {
+      const obj = { ...product, [e.target.name]: Number(e.target.value) };
+      setProduct(obj);
+    } else {
+      const obj = { ...product, [e.target.name]: e.target.value };
+      setProduct(obj);
+    }
+  };
+  const handleClick = () => {
+    editProduct(id, product);
+  };
+  useEffect(() => {
+    getOneProduct(id);
+  }, []);
+  useEffect(() => {
+    if (oneProduct) {
+      setProduct(oneProduct);
+    }
+  }, [oneProduct]);
   return (
     <div>
-      
+      <h1>ADMIN PAGE: EDIT</h1>
+      <input name="title" label="title" onChange={handleInput} type="text" />
+      <input
+        name="description"
+        label="description"
+        onChange={handleInput}
+        type="text"
+      />
+      <input
+        name="category"
+        label="category"
+        onChange={handleInput}
+        type="text"
+      />
+      <input name="price" label="Price" onChange={handleInput} type="number" />
+      <input
+        name="image"
+        label="Image URL"
+        onChange={handleInput}
+        type="text"
+      />
+      <button onClick={handleClick}>Update Product</button>
     </div>
-  )
+  );
 }
--- src/router/MainRoutes.jsx
+++ src/router/MainRoutes.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import HomePage from '../components/pages/HomePage'
-import ProductListPage from '../components/product/ProductListPage'
-import AddProductPage from '../components/product/AddProductPage'
-import EditProductsPage from '../components/product/EditProductsPage'
-import DetailProductPage from '../components/product/DetailProductPage'
-import { Route, Routes } from 'react-router-dom'
+import React from "react";
+import HomePage from "../components/pages/HomePage";
+import ProductListPage from "../components/product/ProductListPage";
+import AddProductPage from "../components/product/AddProductPage";
+import EditProductsPage from "../components/product/EditProductsPage";
+import DetailProductPage from "../components/product/DetailProductPage";
+import { Route, Routes } from "react-router-dom";
 
 export default function MainRoutes() {
-    const PUBLIC_ROUTES = [
-        {path: "/", element:<HomePage />},
-        {path: "/list", element:<ProductListPage />},
-        {path: "/add", element:<AddProductPage />},
-        {path: "/edit/:id", element:<EditProductsPage />},
-        {path: "/detail/:id", element:<DetailProductPage />},
-    ]
+  const PUBLIC_ROUTES = [
+    { path: "/", element: <HomePage /> },
+    { path: "/list", element: <ProductListPage /> },
+    { path: "/add", element: <AddProductPage /> },
+    { path: "/edit/:id", element: <EditProductsPage /> },
+    { path: "/detail/:id", element: <DetailProductPage /> },
+  ];
 
   return (
     <>
-        <Routes>
-            {PUBLIC_ROUTES.map((elem) => (
-                <Route key={elem.path} element={elem.element} />
-            ))}
-        </Routes>
+      <Routes>
+        {PUBLIC_ROUTES.map((elem) => (
+          <Route path={elem.path} key={elem.path} element={elem.element} />
+        ))}
+      </Routes>
     </>
-  )
+  );
 }
