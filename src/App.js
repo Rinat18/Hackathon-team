6428b5d4d635c6e9661d50893521f11d83ed7f2c
Aidar <ajydarbakytov@gmail.com>
2024-02-20 13:52:39 +0600

    sideBar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navbar from "./components/home/Navbar";
 import MainRoutes from "./router/MainRoutes";
 import Footer from "./components/home/Footer";
 import HomePage from "./components/pages/HomePage";
+import SideBar from "./components/product/SideBar";
 
 export default function App() {
   return (
     <div>
-      <HomePage />
+      <SideBar />
+
       <Navbar />
       <MainRoutes />
       <Footer />
--- src/components/product/SideBar.jsx
+++ src/components/product/SideBar.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import React from "react";
+import "./products.scss";
 export default function SideBar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="containerSideBar">
+        <input type="text" placeholder="Я ищу..." />
+
+        <div className="containerSiderBar__title">
+          <h3>Тип аккаунта</h3>
+          <div className="containerSideBar-title__checkbox">
+            <label for="accaunt">
+              <input type="checkbox" id="accaunt" name="accaunt" />
+              Аккаунт
+            </label>
+            <label for="key">
+              <input type="checkbox" id="key" name="key" />
+              Лицинзионный ключ
+            </label>
+            <label for="activation">
+              <input type="checkbox" id="activation" name="activation" />
+              Активация
+            </label>
+            <label for="sartif">
+              <input type="checkbox" id="sartif" name="sartif" />
+              Лицинзионный ключ
+            </label>
+            <label for="activ">
+              <input type="checkbox" id="activ" name="activ" />
+              Активация
+            </label>
+          </div>
+        </div>
+        <button>Сбросить фильтры</button>
+      </div>
+    </>
+  );
 }
